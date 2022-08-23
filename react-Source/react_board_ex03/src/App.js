82bd54db3f4bb12e34580089925f02673c0a2528
gisungHwang <gshwang00@naver.com>
2022-08-23 10:09:51 +0900

    08.23

--- react-Source/react_board_ex03/src/App.js
+++ react-Source/react_board_ex03/src/App.js
@@ -1,192 +1,52 @@
-// import BoardList from "./BoardList";
-// import BoardWrite from "./BoardWrite";
-// import BoardDetail from "./BoardDetail";
-// import BoardUpdateForm from "./BoardUpdateForm";
-// import { useState } from "react";
-// import axios from "axios";
-
-// function App() {
-//   const [boardlist, setBoardlist] = useState({
-//     boardList: [],
-//   });
-
-//   const [article, setArticle] = useState({
-//     board_num: 0,
-//     board_writer: "",
-//     board_title: "",
-//     board_content: "",
-//     board_date: "",
-//   });
-
-//   // 0:글쓰기, 1:상세보기, 2:글수정
-//   const [actionMode, setActionMode] = useState({ mode: 0 });
-
-//   // 글목록
-//   const getList = () => {
-//     // alert("getList(actionMode) =>" + actionMode.mode);
-//     axios
-//       .get("http://localhost:8008/list", {})
-//       .then((res) => {
-//         console.log("res ==>", res);
-//         const { data } = res;
-//         console.log("data ==>", data);
-//         setBoardlist({
-//           boardList: data,
-//         });
-//         setActionMode({
-//           ...actionMode,
-//           mode: 0, // 상세보기
-//         });
-//       })
-//       .catch((e) => {
-//         console.error(e);
-//       });
-//   };
-
-//   // 상세보기
-//   const handleDetail = (e) => {
-//     // alert("handleDetail(actionMode) =>" + actionMode.mode);
-//     axios
-//       .post("http://localhost:8008/detail", { num: e.target.id })
-//       .then((res) => {
-//         const { data } = res;
-//         console.log("detail =>", data);
-//         if (res.data.length > 0) {
-//           setArticle({
-//             ...article,
-//             board_num: data[0].BOARD_NUM,
-//             board_writer: data[0].BOARD_WRITER,
-//             board_title: data[0].BOARD_TITLE,
-//             board_content: data[0].BOARD_CONTENT,
-//             board_date: data[0].BOARD_DATE,
-//           });
-
-//           setActionMode({
-//             ...actionMode,
-//             mode: 1, // 상세보기
-//           });
-//         }
-//       })
-//       .catch((e) => {
-//         console.error(e);
-//       });
-//   };
-
-//   // 수정폼 보기
-//   const handleUpdateForm = (e) => {
-//     alert(
-//       "handleUpdateForm(actionMode) =>" + actionMode.mode + ", " + e.target.id
-//     );
-//     axios
-//       .post("http://localhost:8008/detail", { num: e.target.id })
-//       .then((res) => {
-//         const { data } = res;
-//         console.log("handleUpdateForm =>", data);
-//         if (res.data.length > 0) {
-//           setArticle({
-//             ...article,
-//             board_num: data[0].BOARD_NUM,
-//             board_writer: data[0].BOARD_WRITER,
-//             board_title: data[0].BOARD_TITLE,
-//             board_content: data[0].BOARD_CONTENT,
-//             board_date: data[0].BOARD_DATE,
-//           });
-
-//           setActionMode({
-//             ...actionMode,
-//             mode: 2, // 글수정하기
-//           });
-//         }
-//       })
-//       .catch((e) => {
-//         console.error(e);
-//       });
-//   };
-
-//   const handleUpdate = () => {
-//     console.log("handleUpdate =>", article);
-//     axios
-//       .post("http://localhost:8008/update", {
-//         article: article,
-//       })
-//       .then((res) => {
-//         console.log("handleUpdate( changedRows) =>", res.data.changedRows);
-//         getList();
-//       })
-//       .catch((e) => {
-//         console.error(e);
-//       });
-//   };
-
-//   if (actionMode.mode === 0) {
-//     // alert("글쓰기");
-//     // 글쓰기
-//     return (
-//       <div>
-//         <BoardWrite handlelist={getList}></BoardWrite>
-//         <br />
-//         <BoardList
-//           boardlist={boardlist}
-//           actionmode={actionMode}
-//           handlelist={getList}
-//           handledetail={handleDetail}
-//           handleupdateform={handleUpdateForm}
-//         ></BoardList>
-//       </div>
-//     );
-//   } else if (actionMode.mode === 1) {
-//     // alert("상세정보");
-//     // 상세보기
-//     return (
-//       <div>
-//         <BoardDetail article={article} handlelist={getList}></BoardDetail>
-//         <br />
-//         <BoardList
-//           boardlist={boardlist}
-//           handlelist={getList}
-//           handledetail={handleDetail}
-//           handleupdateform={handleUpdateForm}
-//         ></BoardList>
-//       </div>
-//     );
-//   } else if (actionMode.mode === 2) {
-//     // alert("글수정");
-//     // 글수정
-//     return (
-//       <div>
-//         <BoardUpdateForm
-//           article={article}
-//           setarticle={setArticle}
-//           handleupdate={handleUpdate}
-//         ></BoardUpdateForm>
-//         <br />
-//         <BoardList
-//           boardlist={boardlist}
-//           handlelist={getList}
-//           handledetail={handleDetail}
-//           handleupdateform={handleUpdateForm}
-//         ></BoardList>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-
-import {Route, Routes} from "react-router-dom";
-import Main from "./Main";
-import LoginForm from "./LoginForm";
-import MemberForm from "./MemberForm";
+import { Route, Routes } from 'react-router-dom'
+import LoginForm from './login/LoginForm';
+import MemberForm from './login/MemberForm';
+import StoreMemberForm from './login/StoreMemberForm';
+import StoreLoginForm from './login/StoreLoginForm';
+import FoodCategory from './foodCategory/FoodCategory';
+import MenuChicken from "./menuSite/MenuChicken";
+import MenuCafe from "./menuSite/MenuCafe";
+import MenuPorkfood from "./menuSite/MenuPorkfood";
+import MenuPizza from "./menuSite/MenuPizza";
+import MenuBurger from "./menuSite/MenuBurger";
+import MenuChina from "./menuSite/MenuChina";
+import MenuJapan from "./menuSite/MenuJapan";
+import MenuKfood from "./menuSite/MenuKfood";
+import MenuDessert from "./menuSite/MenuDessert";
+import MenuSandwich from "./menuSite/MenuSandwich";
+import Loading from './Loading/Loading';
+import StoreMain from './storeBoards/StoreMain';
+import MainHosik from './hosik_boards/Main';
+import BoardWriteHosik from './hosik_boards/BoardWrite';
+import MainBbq from './bbq_boards/Main';
+import BoardWriteBbq from './bbq_boards/BoardWrite';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginForm/>} />
-      <Route path="/main" element={<Main/> } />
-      <Route path="/member" element={<MemberForm/>} />  
+      <Route path='/' element={<Loading />} />
+      <Route path='/login' element={<LoginForm />} />
+      <Route path='/member' element={<MemberForm />} />
+      <Route path='/storelogin' element={<StoreLoginForm />} />
+      <Route path='/storemember' element={<StoreMemberForm />} />
+      <Route path="/main" element={<FoodCategory />} />
+      <Route path="/main/menuchicken" element={<MenuChicken />} />
+      <Route path="/main/menucafe" element={<MenuCafe />} />
+      <Route path="/main/menuporkfood" element={<MenuPorkfood />} />
+      <Route path="/main/menupizza" element={<MenuPizza />} />
+      <Route path="/main/menuchina" element={<MenuChina />} />
+      <Route path="/main/menujapan" element={<MenuJapan />} />
+      <Route path="/main/menukfood" element={<MenuKfood />} />
+      <Route path="/main/menudessert" element={<MenuDessert />} />
+      <Route path="/main/menuburger" element={<MenuBurger />} />
+      <Route path="/main/menusandwich" element={<MenuSandwich />} />
+      <Route path='/storeboard' element={<StoreMain />} />
+      <Route path='/boardlisthosik' element={<MainHosik />} />
+      <Route path='/boardwritehosik' element={<BoardWriteHosik />} />
+      <Route path='/boardlistbbq' element={<MainBbq />} />
+      <Route path='/boardwritebbq' element={<BoardWriteBbq />} />
     </Routes>
   )
-}
+};
 
 export default App;
