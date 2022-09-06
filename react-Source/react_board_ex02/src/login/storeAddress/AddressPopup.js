35f011ddc8e9a151fc295ceffcf9fc6eefc993bc
gisungHwang <gshwang00@naver.com>
2022-09-06 10:31:03 +0900

    09.06

--- react-Source/react_board_ex02/src/login/storeAddress/AddressPopup.js
+++
@@ -1,8 +0,0 @@
-import ReactDom from 'react-dom';
-
-const AddressPopup = ({ children }) => {
-  const el = document.getElementById('popupDom');
-  return ReactDom.createPortal(children, el);
-};
-
-export default AddressPopup;
