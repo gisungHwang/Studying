0b662b2c29674cb025f0c7d891b1903c2241bb4c
gisungHwang <108442454+gisungHwang@users.noreply.github.com>
2023-01-11 21:46:46 +0900

    C++

---
+++ Project38/Project47/소스.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+	int num = 20;
+	cout << "Hello World!" << endl;
+	cout << "Hellooooo " << "World!" << endl;
+	cout << num << ' ' << 'b';
+	cout << ' ' << 3.19 << endl;
+	return 0;
+}
