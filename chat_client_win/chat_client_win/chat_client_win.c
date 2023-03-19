5fc94f76f766b4ca33c1e19f1cc75e662284c5ac
gisungHwang <108442454+gisungHwang@users.noreply.github.com>
2023-03-19 23:26:26 +0900

    TCP/IP

--- chat_client_win/chat_client_win/chat_client_win.c
+++ chat_client_win/chat_client_win/chat_client_win.c
@@ -10,8 +10,8 @@ void ErrorHandling(const char *message);
 int main(int argc, char *argv[])
 {
 	WSADATA wsaData;
-	SOCKET hSocket;
-	char message[BUF_SIZE];
+	SOCKET hSocket1;
+	char message1[BUF_SIZE];
 	int strLen;
 	SOCKADDR_IN servAdr;
 
@@ -23,9 +23,10 @@ int main(int argc, char *argv[])
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		ErrorHandling("WSAStartup() error!");
 
-	hSocket = socket(PF_INET, SOCK_STREAM, 0);
-	printf("hSocket : %d\n", hSocket);
-	if (hSocket == INVALID_SOCKET)
+	hSocket1 = socket(PF_INET, SOCK_STREAM, 0);
+	printf("PF_INET : %d\n", PF_INET);
+	printf("hSocket : %d\n", hSocket1);
+	if (hSocket1 == INVALID_SOCKET)
 		ErrorHandling("socket() error");
 
 	memset(&servAdr, 0, sizeof(servAdr));
@@ -35,30 +36,30 @@ int main(int argc, char *argv[])
 	servAdr.sin_port = htons(atoi(argv[2]));
 	printf("port : %s\n", argv[2]);
 
-	if (connect(hSocket, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+	if (connect(hSocket1, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
 		ErrorHandling("connect() error!");
 	else
 		puts("Connected......");
 
 	while (1)
 	{
-		fputs("Input message(Q to quit): ", stdout);
-		fgets(message, BUF_SIZE, stdin);
+		fputs("Chat_Input message(Q to quit): ", stdout);
+		fgets(message1, BUF_SIZE, stdin);
 		printf("BUF_SIZE  : %d\n", BUF_SIZE);
 		printf("stdout  : %d\n", stdout);
 		printf("stdin  : %d\n", stdin);
 
-		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+		if (!strcmp(message1, "q\n") || !strcmp(message1, "Q\n"))
 			break;
 
-		send(hSocket, message, strlen(message), 0);
-		strLen = recv(hSocket, message, BUF_SIZE - 1, 0);
-		printf("strlen(message1) : %d\n", strlen(message));
-		message[strLen] = 0;
-		printf("Message from server: %s\n", message);
+		send(hSocket1, message1, strlen(message1), 0);
+		printf("strlen(message1) : %d\n", strlen(message1));
+		strLen = recv(hSocket1, message1, BUF_SIZE - 1, 0);
+		message1[strLen] = 0;
+		printf("Message from server: %s\n", message1);
 	}
 
-	closesocket(hSocket);
+	closesocket(hSocket1);
 	WSACleanup();
 	return 0;
 }
---
+++ echo_mpserv_win/echo_mpserv_win/echo_mpserv_win.c
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <winsock2.h>
+#include <process.h>
+
+#define BUF_SIZE 30
+void ErrorHandling(char *message);
+void ReadChildproc(int sig);
+
+int main(int argc, char *argv[])
+{
+	WSADATA wsaData;
+	SOCKET servSock, clntSock;
+	struct sockaddr_in servAdr, clntAdr;
+
+	HANDLE pid;
+	int strLen;
+	char buf[BUF_SIZE];
+	if (argc != 2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error");
+
+
+	servSock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi(argv[1]));
+	printf("port : %s\n", argv[1]);
+
+	if (bind(servSock, (struct sockaddr*) &servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+		ErrorHandling("bind() error");
+
+	if (listen(servSock, 5) == SOCKET_ERROR)
+		ErrorHandling("listen() error");
+
+	while (1)
+	{
+		int clntAdrSz = sizeof(clntAdr);
+		//printf("clntAdrSz : %d\n", clntAdrSz);
+		clntSock = accept(servSock, (struct sockaddr*)&clntAdr, &clntAdrSz);
+
+		if (clntSock == INVALID_SOCKET)
+			continue;
+		else
+			puts("new client connected...");
+
+		pid = (HANDLE)_beginthreadex(NULL, 0, (void*)ReadChildproc, (LPVOID)clntSock, 0, NULL);
+		if (pid == NULL)
+		{
+			closesocket(clntSock);
+			continue;
+		}
+	}
+	closesocket(servSock);
+	WSACleanup();
+	return 0;
+}
+
+void ReadChildproc(int clntSock)
+{
+	int strLen;
+	char buf[BUF_SIZE];
+	while ((strLen = recv(clntSock, buf, BUF_SIZE, 0)) != 0)
+	{
+		printf("clntSock : %d  ", clntSock);
+		printf("message : %s\n", buf);
+		send(clntSock, buf, strLen, 0);
+	}
+
+	closesocket(clntSock);
+	puts("client disconnected...");
+	return;
+}
+
+void ErrorHandling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
